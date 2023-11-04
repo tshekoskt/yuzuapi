e61a084580e343165503b57f63a6945aaf8c4804
Theolimour <102794448+Theolimour@users.noreply.github.com>
2023-11-04 22:32:40 +0200

    added isRented to the schema

--- models/rentalProducts.js
+++ models/rentalProducts.js
@@ -1,37 +1,38 @@
 const mongoose = require('mongoose');
 
 const rentalProductSchema = new mongoose.Schema({
-    make: String,
-    model: String,
-    status: String,
-    address: String,
-    categories: String,
-    SubCategories: String,
-    description: String,
-    deliveryOption: String,
-    price: String,
-    year: Number,
-    available: Boolean,
-    startdate: Date,
-    enddate: Date,
-    photos: [String],
-    pictures: [String],
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Category",
-    },
-    rentedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
-    postedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
-    deliveryoption: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "DeliveryOption"
-    }
+  make: String,
+  model: String,
+  status: String,
+  address: String,
+  categories: String,
+  SubCategories: String,
+  description: String,
+  deliveryOption: String,
+  price: String,
+  year: Number,
+  available: Boolean,
+  isRented: Boolean,
+  startdate: Date,
+  enddate: Date,
+  photos: [String],
+  pictures: [String],
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  rentedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  deliveryoption: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "DeliveryOption"
+  }
 });
 
 module.exports = mongoose.model('RentalProduct', rentalProductSchema);
