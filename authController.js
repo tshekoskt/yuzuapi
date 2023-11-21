8c5b4b87b3d34bce56710c6438032e5b6ed54de0
@Nathi860 <nkosinathit70@gmail.com>
2023-11-21 22:14:18 +0200

    login_fix

--- authController.js
+++ authController.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
 const randomstring = require("randomstring");
