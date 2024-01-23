ad99e0246a1e10cff307b8d6545cf6bd8bab29d5
theolimour <motheo@pulego.co.za>
2024-01-23 18:56:46 +0200

    added functionality to send automated email to user after logging query

--- emailService.js
+++ emailService.js
@@ -1,74 +1,70 @@
 const nodemailer = require("nodemailer");
 const constants = require('./constants');
+const fs = require('fs');
+const path = require('path');
 
 class EmailService {
 
-  async sendCancelationEmail(to, body, subject) {
-
-    var transporter = nodemailer.createTransport({
+  constructor() {
+    this.transporter = nodemailer.createTransport({
       pool: true,
       host: 'smtp-relay.brevo.com',
       port: 587,
-      secure: false, // or true if required
+      secure: false,
       auth: {
-        user: constants.YUZU_EMAIL, //'yuzuxapp@gmail.com',
-        pass: constants.YUZU_PASS //'Lb4mqJKWz9BxhcHY',
+        user: constants.YUZU_EMAIL,
+        pass: constants.YUZU_PASS,
       },
     });
+  }
 
+  async sendQueryEmail(name, email, data, subject) {
     const mailOptions = {
-      from: constants.YUZU_EMAIL, // 'yuzuxapp@gmail.com',
-      to: to,
-      subject: subject,
+      from: constants.YUZU_EMAIL, // sender address
+      to: email, // receiver address
+      subject: subject, // Subject line
+      html: data // html body
+    };
+
+    try {
+      let info = await this.transporter.sendMail(mailOptions);
+      console.log('Message sent: %s', info.messageId);
+    } catch (error) {
+      console.error('Error occurred while sending email:', error);
+    }
+  }
+
+  async sendEmail(options) {
+    try {
+      const info = await this.transporter.sendMail(options);
+      this.transporter.close();
+      return { code: 200, message: 'Success', info };
+    } catch (error) {
+      console.error(error);
+      this.transporter.close();
+      throw new Error('Failed to send email');
+    }
+  }
+
+  async sendCancelationEmail(to, body, subject) {
+    const mailOptions = {
+      from: constants.YUZU_EMAIL,
+      to,
+      subject,
       text: body,
     };
-
-    transporter.sendMail(mailOptions, async function (error, info) {
-      if (error) {
-        console.error(error);
-        return { code: 500, message: 'Failed to send email' }
-
-      } else {
-        transporter.close();
-        return { code: 200, message: 'Success' }
-      }
-
-    });
+    return this.sendEmail(mailOptions);
   }
-  
+
   async sendReviewHtmlBody(to, body, subject) {
-
-    var transporter = nodemailer.createTransport({
-      pool: true,
-      host: 'smtp-relay.brevo.com',
-      port: 587,
-      secure: false, // or true if required
-      auth: {
-        user: constants.YUZU_EMAIL, //'yuzuxapp@gmail.com',
-        pass: constants.YUZU_PASS //'Lb4mqJKWz9BxhcHY',
-      },
-    });
-
     const mailOptions = {
-      from: constants.YUZU_EMAIL, // 'yuzuxapp@gmail.com',
-      to: to,
-      subject: subject,
+      from: constants.YUZU_EMAIL,
+      to,
+      subject,
       html: body,
     };
-
-    transporter.sendMail(mailOptions, async function (error, info) {
-      if (error) {
-        console.error(error);
-        return { code: 500, message: 'Failed to send email' }
-
-      } else {
-        transporter.close();
-        return { code: 200, message: 'Success' }
-      }
-
-    });
+    return this.sendEmail(mailOptions);
   }
-  
 }
 
-module.exports = EmailService
+module.exports = EmailService;
